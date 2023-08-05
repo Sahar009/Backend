47712cfe120aa12df17fba42cf587908c50742ac
Akinwumi sehinde <102046091+Sahar009@users.noreply.github.com>
2023-08-05 16:26:32 +0100

    Update Server.js

--- Server.js
+++ Server.js
@@ -31,7 +31,7 @@ app.use(cookieParser())
 app.use(express.urlencoded({extended:false}))
 app.use(bodyParser.json())
 app.use(cors({
-    origin: ['http://localhost:3000','parach-app.vercel.app'],
+    origin: ['http://localhost:3000','https://parach-mgt.vercel.app/'],
     credentials:true
 }))
 
