d74972d9ed29019a245d4564eee3d991db84e2d5
Akinwumi sehinde <102046091+Sahar009@users.noreply.github.com>
2023-12-22 10:21:31 +0100

    Update server.js

--- src/server.js
+++ src/server.js
@@ -31,7 +31,7 @@ app.use(cookieParser())
 app.use(express.urlencoded({extended:false}))
 app.use(bodyParser.json())
 app.use(cors({
-    origin: ['http://localhost:3000','https://parach-mgt.vercel.app','https://wham-iota.vercel.app','https://wham-sahar009.vercel.app'],
+    origin: ['http://localhost:3000','https://parach-mgt.vercel.app','https://wham-iota.vercel.app','https://wham-sahar009.vercel.app','https://dashboard-teal-five.vercel.app'],
     credentials:true,
 }))
 
